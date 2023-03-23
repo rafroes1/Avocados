1c54d02da937cfe2054de5522269dab55308d9a8
rafroes1 <rafafroes@outlook.com>
2023-03-23 10:49:03 -0300

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Rafael Carvalho on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
